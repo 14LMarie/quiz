f3b7949f399a4afbc543d73b3d6516e1914052c4
Lynsey Teague <lynsey.teague@yahoo.com>
2016-01-19 03:15:21 -0500

    non-interactive version

---
+++ js/app.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+            var questions = [
+    //question 1
+                {
+                    question: 'What NFL football team calls Charlotte home?',
+                    choices: ['Panthers', 'Seahawks', 'Steelers', 'Packers'],
+                    correct: 0,
+   },
+    //question 2
+                {
+                    question: 'What NBA team resides in Charlotte?',
+                    choices: ['Bulls', 'Hornets', 'Cavaliers', 'Heat'],
+                    correct: 1,
+    },
+
+
+    //question 3
+                {
+                    question: 'Charlotte was named after which British Queen?',
+                    choices: ['Charlotte Diane', 'Charlotte Octavia', 'Charlotte Elizabeth', 'Charlotte Sophia'],
+                    correct: 3,
+    },
+
+    //question 4
+                {
+                    question: 'Which mega church was started in Charlotte?',
+                    choices: ['Elevation', 'Jesus Church', 'United', 'Life Church'],
+                    correct: 0,
+    },
+
+    //question 5
+                {
+                    question: 'Charlotte is know for major contributions to which industry?',
+                    choices: ['Information Technology', 'Textile', 'Finance', 'Marketing'],
+                    correct: 2,
+    },
+
+    //question 6
+                {
+                    question: 'Which amusement park is in Charlotte?',
+                    choices: ['Carowinds', 'Six Flags', 'Hershey Park', 'Animal Kingdom'],
+                    correct: 0,
+    },
+
+    //question 7
+                {
+                    question: 'The most fabulous art district of Charlotte is?',
+                    choices: ['Noda', 'Plaza Midwood', 'Southend', 'University'],
+                    correct: 1,
+    },
+
+    //question 8
+                {
+                    question: 'Which city is the largest in North Carolina?',
+                    choices: ['Raleigh', 'Charlotte', 'Charleston', 'Myrtle Beach'],
+                    correct: 1,
+    },
+
+]
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+        //display questions with answer choices
+        //get user input and validate
+        //feedback if correct or incorrect
+        //function to count correct and incorrect responses
+        //function to end and start quiz over
